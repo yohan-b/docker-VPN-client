451bc1028a9464651455e175ecf63d53631b8896
yohan <783b8c87@scimetis.net>
2019-11-03 16:34:01 +0100

    Added L2TP route.

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,6 +29,8 @@ trap "shut_down" SIGINT SIGTERM SIGKILL
 start_openvpn
 sleep 3
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+# L2TP route
+ip route add 192.168.2.0/24 via 172.31.1.3 dev eth0
 #ip route add 192.168.1.0/24 via 192.168.2.1 dev eth0
 #bash
 
