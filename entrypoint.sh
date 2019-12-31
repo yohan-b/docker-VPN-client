2fee11611b14c9257599d97fca0ec804162adb9d
yohan <783b8c87@scimetis.net>
2019-12-31 19:21:10 +0100

    Externalized custom post-up commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,11 +28,7 @@ trap "shut_down" SIGINT SIGTERM SIGKILL
 # start service in background here
 start_openvpn
 sleep 3
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-# L2TP route
-ip route add 192.168.2.0/24 via 172.31.1.3 dev eth0
-#ip route add 192.168.1.0/24 via 192.168.2.1 dev eth0
-#bash
+test -f /root/post-up.sh && /root/post-up.sh
 
 while true
 do
